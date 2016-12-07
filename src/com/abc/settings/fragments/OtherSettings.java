e905590984d54606ef4a5f32bdf673a5fd728969
ezio84 <brabus84@gmail.com>
2016-12-07 21:52:26 +0100

    Allow to customize max SMS per minute limit [1/2]
    Aosp has a fixed 30sms per minute value, stored in the Settings.java:
           public static final String SMS_OUTGOING_CHECK_MAX_COUNT =
                   "sms_outgoing_check_max_count";
    
    and src/java/com/android/internal/telephony/SmsUsageMonitor.java
    in the frameworks/opt/telephony:
        /** Default number of SMS sent in checking period without user permission. */
        private static final int DEFAULT_SMS_MAX_COUNT = 30;
    
    Remove the pref if the device isn't a phone.
    
    The 2/2 part is in the SmsUsageMonitor class, needed to avoid a reboot to
    update the Global Setting, and it was already made by CM guys:
    https://github.com/ezio84/abc-n_frameworks_opt_telephony/commit/b3be4cbc075c546470d4b0f01ea040e3d1046aaf

--- src/com/abc/settings/fragments/OtherSettings.java
+++ src/com/abc/settings/fragments/OtherSettings.java
@@ -18,8 +18,10 @@ package com.abc.settings.fragments;
 import android.content.ContentResolver;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen;
 import android.provider.Settings;
 
@@ -27,23 +29,66 @@ import com.android.internal.logging.MetricsProto.MetricsEvent;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+import java.util.List;
+import java.util.ArrayList;
 
 public class OtherSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
+
+    private static final String APPS_SECURITY = "apps_security";
+    private static final String SMS_OUTGOING_CHECK_MAX_COUNT = "sms_outgoing_check_max_count";
+
+    private ListPreference mSmsCount;
+    private int mSmsCountValue;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.abc_other_settings);
+        PreferenceScreen prefScreen = getPreferenceScreen();
+        ContentResolver resolver = getActivity().getContentResolver();
 
+        PreferenceCategory appsSecCategory = (PreferenceCategory) findPreference(APPS_SECURITY);
+
+        mSmsCount = (ListPreference) findPreference(SMS_OUTGOING_CHECK_MAX_COUNT);
+        mSmsCountValue = Settings.Secure.getInt(resolver,
+                Settings.Global.SMS_OUTGOING_CHECK_MAX_COUNT, 30);
+        mSmsCount.setValue(Integer.toString(mSmsCountValue));
+        mSmsCount.setSummary(mSmsCount.getEntry());
+        mSmsCount.setOnPreferenceChangeListener(this);
+        if (!Utils.isVoiceCapable(getActivity())) {
+            appsSecCategory.removePreference(mSmsCount);
+            prefScreen.removePreference(appsSecCategory);
+        }
+
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        // If we didn't handle it, let preferences handle it.
+        return super.onPreferenceTreeClick(preference);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        if (preference == mSmsCount) {
+            mSmsCountValue = Integer.valueOf((String) newValue);
+            int index = mSmsCount.findIndexOfValue((String) newValue);
+            mSmsCount.setSummary(
+                    mSmsCount.getEntries()[index]);
+            Settings.Global.putInt(resolver,
+                    Settings.Global.SMS_OUTGOING_CHECK_MAX_COUNT, mSmsCountValue);
+            return true;
+        }
+        return false;
     }
 
     @Override
     protected int getMetricsCategory() {
         return MetricsEvent.ABC;
     }
-
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        return false;
-    }
-} 
+}
